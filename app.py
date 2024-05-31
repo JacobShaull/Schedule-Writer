bfe7880427f9346a5c8fa44c6f3d215c50756352
lilsharkie <jacobshaull47@gmail.com>
2024-05-31 01:44:13 -0700

    added .py files

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
 
 
 if __name__ == '__main__':
